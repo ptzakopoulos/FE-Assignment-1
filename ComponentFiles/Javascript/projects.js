b93c2f90ed2f5a740b19b0ea803c0f7e7bef2e6f
Tzakopoulos Panagiotis <115104147+ptzakopoulos@users.noreply.github.com>
2022-11-02 19:00:39 +0200

    Prijects Styling

---
+++ ComponentFiles/Javascript/projects.js
@@ -0,0 +1,12 @@
+//@ts-check
+'use strict';
+window.onload = () => {
+  // ~~~~~~~~~~ Element Variables ~~~~~~~~~~
+  const allBt = document.getElementById('all');
+  const faceialBt = document.getElementById('facial');
+  const bodyBt = document.getElementById('body');
+  const bwBt = document.getElementById('bw');
+  const coloredBt = document.getElementById('colored');
+
+  allBt.onclick = () => {};
+};
